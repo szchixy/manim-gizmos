5d83079ab28c10f2bda9d40a3e107adc7d3f2833
szchixy <szchixy@outlook.com>
2023-12-03 17:10:05 +0800

    1. remove manim.cfg 2. add my_config.py to set default variable

---
+++ my_config.py
@@ -0,0 +1,1 @@
+DEFAULT_TEXT_FONT = 'Consolas-with-Yahei'
--- start.py
+++ start.py
@@ -1,12 +1,13 @@
 from manim import *
 from this import d, s
+from my_config import DEFAULT_TEXT_FONT
 
 class ZenOfPython(Scene):
     def construct(self):
         string = "".join([d.get(c, c) for c in s]).replace('\n\n', '\n').strip()
         line_lst = string.strip().split('\n')
-        text_lst = [Text(line, font_size=36) for line in line_lst]
-        footer = Text('Created by szchixy, Powered by manimgl', font_size=20)
+        text_lst = [Text(line, font=DEFAULT_TEXT_FONT, font_size=28) for line in line_lst]
+        footer = Text('Created by szchixy, Powered by manimgl', font=DEFAULT_TEXT_FONT, font_size=16)
         self.play(Write(text_lst[0]))
         self.wait()
         self.play(FadeOut(text_lst[0]))
